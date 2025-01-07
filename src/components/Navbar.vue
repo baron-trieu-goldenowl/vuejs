11e3f50132fb926931986ff6f0ad70163fec0725
Baron Trieu <tjeu.bao.ouit@gmail.com>
2025-01-07 09:37:48 +0700

    add product

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,9 @@
         <div class="navbar-start">
           <router-link to="/"><a class="nav-item" >Home</a></router-link>
           <a class="nav-item" href="/About">About</a>
+          <a class="nav-item" href="/Products">Product</a>
           <a class="nav-item" href="/Categories">All category</a>
-    
+
           <a class="nav-item" href="/profile"v-if="isLoggedIn" >Profile</a>
           <a class="nav-item" href=""  @click.prevent="logout" v-if="isLoggedIn">Logout</a>
           <a class="nav-item" href="/login" v-else="isLoggedIn">Login</a>
@@ -16,15 +17,15 @@
       </div>
     </nav>
   </template>
-  
+
   <script setup>
   import { defineProps, defineEmits } from 'vue';
   import {useStore} from 'vuex';
   import { useRouter } from 'vue-router';
-  
+
   const store = useStore();
   const router = useRouter();
-  
+
   const props = defineProps({
     isLoggedIn: {
       type: Boolean,
@@ -35,17 +36,16 @@
       required: true,
     },
   });
-  
+
   const emit = defineEmits(['logout']);
-  
+
   const logout = () => {
     store.dispatch('logout').then(() => {
       router.push('/');
     });
   };
   </script>
-  
+
   <style scoped>
-  
+
   </style>
-  
